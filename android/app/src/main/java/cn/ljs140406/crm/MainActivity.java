7b77f7f157098214b05f2cbf534535aac925ade1
ljs140406 <1404062483@qq.com>
2026-09-02 13:54:54 +0800

    sync: android/app/src/main/java/cn/ljs140406/crm/MainActivity.java

--- android/app/src/main/java/cn/ljs140406/crm/MainActivity.java
+++ android/app/src/main/java/cn/ljs140406/crm/MainActivity.java
@@ -1,5 +1,14 @@
 package cn.ljs140406.crm;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 允许 WebView 铺到系统栏下方，配合 CSS env(safe-area-inset-*) 做沉浸式适配
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    }
+}
